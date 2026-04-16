0383947f43b216ee66d70d08a29e92be86e705eb
vaahwhzhzhsbsh-svg <vaahwhzhzhsbsh@gmail.com>
2026-04-16 08:40:32 +0000

    Fix: Systemized build and code structure

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'features/video_conversion/presentation/pages/converter_page.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xFF0a0e27),
-        primaryColor: const Color(0xFF00d9ff),
-        appBarTheme: AppBarTheme(
-          backgroundColor: const Color(0xFF0a0e27),
-          elevation: 0,
-          titleTextStyle: GoogleFonts.elMessiri(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-            color: const Color(0xFF00d9ff),
-          ),
-        ),
-        textTheme: GoogleFonts.elMessiriTextTheme(
-          Theme.of(context).textTheme.apply(
-            bodyColor: Colors.white,
-            displayColor: Colors.white,
-          ),
-        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E27),
       ),
       home: const ConverterPage(),
     );
