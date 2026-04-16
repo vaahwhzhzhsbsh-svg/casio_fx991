ee5a7b5dd5845c2f2d21cc5a94f445cb1af24ccb
vaahwhzhzhsbsh-svg <vaahwhzhzhsbsh@gmail.com>
2026-04-16 08:49:47 +0000

    Fix: Absolute package import for VideoConversionParams

--- lib/features/video_conversion/domain/models/video_params.dart
+++ lib/features/video_conversion/domain/models/video_params.dart
@@ -2,12 +2,10 @@ class VideoConversionParams {
   final String inputPath;
   final String outputPath;
   final String format;
-  final double? quality;
 
   VideoConversionParams({
     required this.inputPath,
     required this.outputPath,
     required this.format,
-    this.quality,
   });
 }
